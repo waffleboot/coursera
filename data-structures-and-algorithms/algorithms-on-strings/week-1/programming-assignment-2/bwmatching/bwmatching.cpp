2584b96a901ae1d45df63e83884500b62e6236fd
Andrei Yangabishev <waffleboot@gmail.com>
2018-06-14 03:15:25 +0300

    implement bw pattern matching

--- data-structures-and-algorithms/algorithms-on-strings/week-1/programming-assignment-2/bwmatching/bwmatching.cpp
+++ data-structures-and-algorithms/algorithms-on-strings/week-1/programming-assignment-2/bwmatching/bwmatching.cpp
@@ -5,12 +5,23 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <array>
 
 using std::cin;
 using std::istringstream;
 using std::map;
 using std::string;
 using std::vector;
+
+size_t LetterToIndex(char ch) {
+  switch (ch) {
+    case '$': return 0;
+    case 'A': return 1;
+    case 'C': return 2;
+    case 'G': return 3;
+    case 'T': return 4;
+  }
+}
 
 // Preprocess the Burrows-Wheeler Transform bwt of some text
 // and compute as a result:
@@ -23,7 +34,45 @@ using std::vector;
 void PreprocessBWT(const string& bwt, 
                    map<char, int>& starts, 
                    map<char, vector<int> >& occ_count_before) {
-  // Implement this function yourself
+  std::array<size_t,5> count {};
+  vector<size_t> occ (count.size());
+  vector<int> occ_0,occ_1,occ_2,occ_3,occ_4;
+  for (auto c : bwt) {
+    ++count[LetterToIndex(c)];
+    occ_0.push_back(occ[LetterToIndex('$')]);
+    occ_1.push_back(occ[LetterToIndex('A')]);
+    occ_2.push_back(occ[LetterToIndex('C')]);
+    occ_3.push_back(occ[LetterToIndex('G')]);
+    occ_4.push_back(occ[LetterToIndex('T')]);
+    ++occ[LetterToIndex(c)];
+  }
+  occ_0.push_back(occ[LetterToIndex('$')]);
+  occ_1.push_back(occ[LetterToIndex('A')]);
+  occ_2.push_back(occ[LetterToIndex('C')]);
+  occ_3.push_back(occ[LetterToIndex('G')]);
+  occ_4.push_back(occ[LetterToIndex('T')]);
+  
+  occ_count_before.emplace('$',std::move(occ_0));
+  occ_count_before.emplace('A',std::move(occ_1));
+  occ_count_before.emplace('C',std::move(occ_2));
+  occ_count_before.emplace('G',std::move(occ_3));
+  occ_count_before.emplace('T',std::move(occ_4));
+
+  starts.emplace('A',count[0]); count[1] += count[0];
+  starts.emplace('C',count[1]); count[2] += count[1];
+  starts.emplace('G',count[2]); count[3] += count[2];
+  starts.emplace('T',count[3]);
+
+//  for (size_t i = 0; i <= bwt.size(); ++i) {
+//    printf("%d: %d %d %d %d %d\n",
+//           i,
+//           occ_count_before['$'][i],
+//           occ_count_before['A'][i],
+//           occ_count_before['C'][i],
+//           occ_count_before['G'][i],
+//           occ_count_before['T'][i]);
+//  }
+//  printf("\n");
 }
 
 // Compute the number of occurrences of string pattern in the text
@@ -33,8 +82,15 @@ int CountOccurrences(const string& pattern,
                      const string& bwt, 
                      const map<char, int>& starts, 
                      const map<char, vector<int> >& occ_count_before) {
-  // Implement this function yourself
-  return 0;
+  size_t top = 0;
+  size_t bottom = bwt.size() - 1;
+  size_t pattern_size = pattern.size();
+  while (top <= bottom && pattern_size > 0) {
+    auto c = pattern[--pattern_size];
+    top = starts.at(c) + occ_count_before.at(c)[top];
+    bottom = starts.at(c) + occ_count_before.at(c)[bottom+1] - 1;
+  }
+  return bottom - top + 1;
 }
      
 
@@ -58,8 +114,8 @@ int main() {
     string pattern;
     cin >> pattern;
     int occ_count = CountOccurrences(pattern, bwt, starts, occ_count_before);
-    printf("%d ", occ_count);
+    printf(pi ? " %d" : "%d", occ_count);
   }
-  printf("\n");
+//  printf("\n");
   return 0;
 }
--- data-structures-and-algorithms/algorithms-on-strings/week-1/programming-assignment-2/run.sh
+++ data-structures-and-algorithms/algorithms-on-strings/week-1/programming-assignment-2/run.sh
@@ -3,7 +3,7 @@
 g++ -pipe -O2 -std=c++14 bwt/bwt.cpp -o bwt/bwt -lm
 
 cat << EOF | bwt/bwt
-ATGC$
+GAGAGAT\$
 EOF
 
 bwt/bwt < bwt/sample_tests/sample1 | diff -a bwt/sample_tests/sample1.a -
@@ -11,23 +11,26 @@ bwt/bwt < bwt/sample_tests/sample2 | diff -a bwt/sample_tests/sample2.a -
 bwt/bwt < bwt/sample_tests/sample3 | diff -a bwt/sample_tests/sample3.a -
 time bwt/bwt < bwt/sample_tests/sample4 > /dev/null
 
-g++ -pipe -O2 -std=c++14 bwtinverse/bwtinverse.cpp -o bwtinverse/bwtinverse -lm 2>/dev/null
+g++ -pipe -O2 -std=c++14 bwtinverse/bwtinverse.cpp -o bwtinverse/bwtinverse -lm
 
 cat << EOF | bwtinverse/bwtinverse
-C\$GTA
+TGGG\$AAA
 EOF
 
 bwtinverse/bwtinverse < bwtinverse/sample_tests/sample1 | diff -a bwtinverse/sample_tests/sample1.a -
 bwtinverse/bwtinverse < bwtinverse/sample_tests/sample2 | diff -a bwtinverse/sample_tests/sample2.a -
 time bwtinverse/bwtinverse < bwtinverse/sample_tests/sample3 > /dev/null
 
-g++ -pipe -O2 -std=c++14 bwmatching/bwmatching.cpp -o bwmatching/bwmatching -lm 2>/dev/null
+g++ -pipe -O2 -std=c++14 bwmatching/bwmatching.cpp -o bwmatching/bwmatching -lm
 
-cat << EOF | bwmatching/bwmatching
-C\$GTA
-EOF
+#cat << EOF | bwmatching/bwmatching
+#TGGG\$AAA
+#1
+#AGA
+#EOF
 
 bwmatching/bwmatching < bwmatching/sample_tests/sample1 | diff -a bwmatching/sample_tests/sample1.a -
 bwmatching/bwmatching < bwmatching/sample_tests/sample2 | diff -a bwmatching/sample_tests/sample2.a -
 bwmatching/bwmatching < bwmatching/sample_tests/sample3 | diff -a bwmatching/sample_tests/sample3.a -
 time bwmatching/bwmatching < bwmatching/sample_tests/sample4 > /dev/null
+
